1f3d20d2bad06011a096f079a0d9d3264c4c2927
lemamon <demmarinho@gmail.com>
2025-03-06 16:27:05 -0300

    feat: replace input and select elements with reusable Input and Select components in Filters

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,55 +1,49 @@
-import React from "react";
 import { useDevice } from "./../../contexts";
 import { useTranslation } from "react-i18next";
 import { DeviceTypeFilter, SORT_OPTIONS } from "../../types";
-import "./styles.css";
+import { Select } from "../Select";
+import { Input } from "../Input";
 
+import "./styles.css";
 
 export const Filters: React.FC = () => {
   const { t } = useTranslation();
 
-    const {
-      search,
-      deviceType,
-      sortBy,
-      setSearch,
-      setDeviceType,
-      setSortBy,
-    } = useDevice();
+  const { search, deviceType, sortBy, setSearch, setDeviceType, setSortBy } =
+    useDevice();
 
   return (
     <div className="filters">
-      <input
+      <Input
         type="text"
         placeholder={t("search")}
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         className="search-input"
+        icon="search"
       />
 
-      <select
+      <Select
         value={deviceType}
         onChange={(e) => setDeviceType(e.target.value)}
+        options={DeviceTypeFilter.map((type: string) => ({
+          key: type,
+          value: type,
+          text: type,
+        }))}
         className="device-type-select"
-      >
-        {DeviceTypeFilter.map((type: string) => (
-          <option key={type} value={type}>
-            {type}
-          </option>
-        ))}
-      </select>
+      />
 
-      <select
+      <Select
         value={sortBy}
         onChange={(e) => setSortBy(e.target.value)}
+        options={SORT_OPTIONS.map((option) => ({
+          key: option,
+          value: option,
+          text: t(option),
+        }))}
         className="sort-select"
-      >
-        {SORT_OPTIONS.map((option) => (
-          <option key={option} value={option}>
-            {t(option)}
-          </option>
-        ))}
-      </select>
+      />
     </div>
   );
 };
