21b196baebc2990cbd48e2b05e84b55498cf2798
lemamon <demmarinho@gmail.com>
2025-03-06 14:55:12 -0300

    feat: wrap App component with DeviceProvider for context management

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from "react-dom/client";
 import App from './App.tsx'
+import { DeviceProvider } from "./contexts/DeviceContext.tsx";
+import "./i18n";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <DeviceProvider>
+      <App />
+    </DeviceProvider>
+  </StrictMode>
+);
