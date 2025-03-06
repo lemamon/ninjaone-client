d9f8952ae397dc17db41dd5c87fdc8d7488f8bda
lemamon <demmarinho@gmail.com>
2025-03-06 14:57:05 -0300

    feat: add Header component with add device button and logo

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { getIcon } from "../../assets";
+import { useTranslation } from "react-i18next";
+import "./styles.css";
+
+interface HeaderProps {
+  onAddDevice: () => void;
+}
+
+export const Header: React.FC<HeaderProps> = ({ onAddDevice }) => {
+  const { t } = useTranslation();
+
+  return (
+    <header className="header">
+      <div className="header-content">
+        <img className="logo" src={getIcon("logo")} />
+        <button className="add-device-btn" onClick={onAddDevice}>
+          <img src={getIcon("plus")} alt="Add device icon" />
+          {t("addDevice")}
+        </button>
+      </div>
+    </header>
+  );
+};
